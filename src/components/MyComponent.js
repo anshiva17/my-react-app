dbe5da0dc33ffd6d491276502fd7713cb9b38ed2
anshiva17 <katochanu55@gmail.com>
2025-02-11 13:32:03 +0530

    Initial commit

---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+// src/components/MyComponent.js
+import React from "react";
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h2>Hello, this is MyComponent!</h2>
+    </div>
+  );
+};
+
+export default MyComponent;
