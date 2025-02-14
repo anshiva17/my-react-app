944a61b49b86d88ea57c273b2e223c89712e1497
anshiva17 <katochanu55@gmail.com>
2025-02-14 19:46:19 +0530

    Normalize line endings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./css/style.css";
 
 import "./App.css";
+import "./components/Navbar/navbar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import MyComponent from "./components/MyComponent";
 import Header from "./components/header";
-import Navbar from "./components/navbar"; // Import Navbar component
+import Navbar from "./components/Navbar/navbar"; // Import Navbar component
+import Carousel from "./components/carousel"; // Import Navbar component
+
 import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter
 
 function App() {
@@ -19,6 +23,7 @@ function App() {
       <Router>
         <Navbar /> {/* Using Navbar inside App */}
       </Router>
+      <Carousel />
     </div>
   );
 }
